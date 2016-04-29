49bea9d7f063461c88fb164cf835e3a474e99c1b
kunesv <kunesv@sprinters.cz>
2016-04-29 18:31:53 +0200

    Setup node and Metalsmith for Heroku

---
+++ metalsmith.js
@@ -0,0 +1,44 @@
+var Metalsmith = require('metalsmith');
+// var collections = require('metalsmith-collections');
+// var markdown = require('metalsmith-markdown');
+// var permalinks = require('metalsmith-permalinks');
+// var dateFormatter = require('metalsmith-date-formatter');
+// var layouts = require('metalsmith-layouts');
+var serve = require('metalsmith-serve');
+var watch = require('metalsmith-watch');
+
+Metalsmith(__dirname)
+    // .use(collections({
+    //     "articles": {
+    //         "pattern": "articles/**/*.*",
+    //         "sortBy": "date",
+    //         "reverse": true
+    //     }
+    // }))
+    // .use(markdown())
+    // .use(permalinks(
+    //     {
+    //         "pattern": ":title",
+    //         "relative": false
+    //     }
+    // ))
+    // .use(dateFormatter({
+    //     "dates": [{
+    //         "key": "date",
+    //         "format": "MMM Do YYYY"
+    //     }]
+    // }))
+    // .use(layouts({
+    //     "engine": "handlebars",
+    //     "directory": "src/layouts"
+    // }))
+    .use(serve({
+        "host": "0.0.0.0",
+        "port": process.env.PORT || 5000
+    }))
+    .use(watch())
+    .build(function (err) {
+        if (err) throw err;
+    });
+
+
